1e79d20e15418e6f693c766a94a5c7379a7e2b28
yuthila_banuka <banukadeseram12@gmail.com>
2025-11-23 21:56:12 +0530

    changed the colors in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,14 +3,21 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-      <div className = 'px-6 py-4 text-black bg-violet-800'>
-        <ul className='flex items-center '>
+      <div className=''>
+
+      </div>
+      <div
+        className='px-6 py-4 text-white'
+        style={{ background: 'linear-gradient(to bottom, #300048, #360037)' }}
+      >
+        <ul className='flex items-center justify-end gap-6'>
             <li className='cursor-pointer hover:text-blue-400'>Home</li>
             <li className='cursor-pointer hover:text-blue-400'>About</li>
             <li className='cursor-pointer hover:text-blue-400'>Contact</li>
             <li className='cursor-pointer hover:text-blue-400'>Project</li>
         </ul>
       </div>
+
     </div>
   )
 }
