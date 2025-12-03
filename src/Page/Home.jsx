5d49c76b6330d89711df9b83a6c0be5f34a758ab
yuthila_banuka <banukadeseram12@gmail.com>
2025-12-03 19:20:59 +0530

    created the navbar

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
-    <Hero/>
+    <Nav/>
 
   )
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import logo from '../assets/logo.png'
+
+export default function Nav() {
+  return (
+  <div className='p-7'>  
+    <div className="rounded-full w-1373 h-16 flex items-center justify-between gap-5 px-6 bg-[#370238] text-white">
+      <img src={logo} className="w-10 rounded-full" />
+        <ul className='flex gap-12 font-Cairo'>
+          <a className='cursor-pointer hover:text-gray-300'>Home</a>
+          <a className='cursor-pointer hover:text-gray-300'>About</a>
+          <a className='cursor-pointer hover:text-gray-300'>Contact</a>
+          <a className='cursor-pointer hover:text-gray-300'>Projects</a>
+        </ul>
+    </div>
+  </div>
+
+  )
+}
