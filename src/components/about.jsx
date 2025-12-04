dc90600e6b1b50f46bd4dfafbf8c834c05e661b4
yuthila_banuka <banukadeseram12@gmail.com>
2025-12-04 20:06:31 +0530

    added about section

---
+++ src/components/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <h1 className='p-10 pb-32 m-4 text-white font-extralight text-7xl font-Poppins text-base/1 '>
+        I’m an undergraduate and currently diving deep into machine learning and deep learning. I’m also building responsive websites, experimenting with creative software ideas, and learning how to design efficient data pipelines.
+      </h1>
+    </div>
+  )
+}
