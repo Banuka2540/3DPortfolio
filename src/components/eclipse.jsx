84a99c786b2132f01f5d42db5ada24caa4797407
yuthila_banuka <banukadeseram12@gmail.com>
2025-12-04 22:43:35 +0530

    added about section and resize the pic

---
+++ src/components/eclipse.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Ellipse from '../assets/mergedastro.png'
+
+
+export default function Eclipse() {
+  return (
+    
+    <div className='pt-0 pl-2 pr-0'>
+        
+        <img src={Ellipse} className='max-w-xl '/>
+        
+    </div>
+  )
+}
